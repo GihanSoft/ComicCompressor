4304effb3d9ef0b75139b3f765cb6bbafeca393e
chiefmb <chief.mb1976@gmail.com>
2018-12-09 09:33:51 +0330

    Setted UI Project Name & implemented FolderSelector with WinForm

--- Gihan.Wpf.FolderSelector/FolderSelector.xaml.cs
+++ Gihan.Wpf.FolderSelector/FolderSelector.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
+
+using FolderBrowserDialog = System.Windows.Forms.FolderBrowserDialog;
 
 namespace Gihan.Wpf
 {
@@ -9,14 +10,29 @@ namespace Gihan.Wpf
     /// </summary>
     public partial class FolderSelector : UserControl
     {
+        public TextBox TextBox { get; }
+        public Button Button { get; }
+        public string DialogTitle { get; set; }
+
         public FolderSelector()
         {
             InitializeComponent();
+            TextBox = TxtPath;
+            Button = BtnBrowse;
         }
 
         private void BtnBrowse_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            //todo make it standard folder select
+            var dialog = new FolderBrowserDialog
+            {
+                Description = DialogTitle,
+            };
+            var result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                TxtPath.Text = dialog.SelectedPath;
+            }
         }
     }
 }
