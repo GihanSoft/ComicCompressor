ab095b2f5239e8d175be04f5cc1ba2c4f6f40e8a
chiefmb <chief.mb1976@gmail.com>
2018-12-09 09:21:11 +0330

    Added Fihan.Wpf.FolderSelector

---
+++ Gihan.Wpf.FolderSelector/FolderSelector.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Gihan.Wpf
+{
+    /// <summary>
+    /// Interaction logic for FolderSelector.xaml
+    /// </summary>
+    public partial class FolderSelector : UserControl
+    {
+        public FolderSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
